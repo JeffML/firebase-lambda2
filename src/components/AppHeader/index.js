b5f6908dca48f5523591885fec12031b9c5d92f4
JeffML <jlowery2663@gmail.com>
2019-09-13 15:49:26 -0700

    WIP

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -12,28 +12,11 @@ const AppHeader = (props) => {
           <div className='app-left-nav'>
             <img src={logo} className='app-logo' alt='logo' />
             <div className='app-title-text'>
-              <h1 className='app-title'>Netlify + Fauna DB</h1>
+              <h1 className='app-title'>Chess Maps</h1>
               <p className='app-intro'>
-                Using FaunaDB & Netlify functions
+                Heat maps that visualize patterns in master chess game collections
               </p>
             </div>
-          </div>
-        </div>
-        <div className='deploy-button-wrapper'>
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-faunadb-example'>
-            <img src={deployButton} className='deploy-button' alt='deploy to netlify' />
-          </a>
-          <div className='view-src'>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://github.com/netlify/netlify-faunadb-example'>
-              <img className='github-icon' src={github} alt='view repo on github' />
-              View the source Luke
-            </a>
           </div>
         </div>
       </div>
