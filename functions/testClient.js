b4c7d6450429460ea3f96be78d24b56be8505c61
JeffML <jlowery2663@gmail.com>
2019-09-20 17:20:41 -0700

    client working

---
+++ functions/testClient.js
@@ -0,0 +1,33 @@
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag'
+
+
+const httpLink = createHttpLink({
+  uri: 'https://graphql.fauna.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: "Basic Zm5BRFgtUnc2NEFDQlNzRnkzbGRGMm9FcHd6Z3F1WlExQzR0Q2VHUzpwZ25iYXNlOnNlcnZlcg=="
+    }
+  }
+});
+
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+})
+
+
+exports.handler = (event, context, callback) => {
+  const allOpenings = gql`    
+  query openings {
+      allOpenings {
+        data {fen}
+      }
+    }
+  `;
+}
