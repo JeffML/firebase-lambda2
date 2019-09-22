3292c033a9416facdda84fa24d7238959b02671c
JeffML <jlowery2663@gmail.com>
2019-09-22 15:55:46 -0700

    new stuff

---
+++ functions/all-opening-fens.js
@@ -0,0 +1,31 @@
+import ApolloClient from 'apollo-boost';
+
+import gql from 'graphql-tag'
+import fetch from 'node-fetch'
+
+const URL = 'https://graphql.fauna.com/graphql'
+// const URL = 'http://localhost:9000/'  //FIXME
+
+const client = new ApolloClient({
+  uri: URL,
+  fetch,
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: "Basic Zm5BRFgtUnc2NEFDQlNzRnkzbGRGMm9FcHd6Z3F1WlExQzR0Q2VHUzpwZ25iYXNlOnNlcnZlcg=="
+      },
+    });
+  },
+})
+
+
+exports.handler = (event, context, callback) => {
+  const allOpeningFens = gql`    
+  query openings {
+      allOpenings {
+        data {fen}
+      }
+    }
+  `;
+  return client.query({ query: allOpeningFens })
+}
---
+++ scripts/scidToJSON.js
@@ -0,0 +1,7 @@
+import ScidParser from '../src/utils/ScidParser.js'
+
+const parser = new ScidParser()
+
+parser.init('./scripts/scid.eco')
+const openings = parser.openings();
+console.dir(openings, { depth: 5 });
