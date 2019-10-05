bbc96f301ee3b8841893c7f8a86d314a215df364
JeffML <jlowery2663@gmail.com>
2019-10-05 16:10:03 -0700

    test firestore working

---
+++ functions/firebaseTest.js
@@ -0,0 +1,18 @@
+import Firestore from '@google-cloud/firestore';
+
+// Create a new client
+const firestore = new Firestore();
+
+async function quickstart() {
+  // Obtain a document reference.
+  const document = firestore.doc('posts/intro-to-firestore');
+
+  // Enter new data into the document.
+  await document.set({
+    title: 'Welcome to Firestore',
+    body: 'Hello World',
+  });
+  console.log('Entered new data into the document');
+}
+
+quickstart();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 var webpack = require("webpack");
 
-/* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
 module.exports = {
   plugins: [
     new webpack.DefinePlugin({ "global.GENTLY": false })
