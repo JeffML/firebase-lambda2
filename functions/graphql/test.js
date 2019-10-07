6bddc1c08b0e3be5b3dd443883312024ee1ec470
JeffML <jlowery2663@gmail.com>
2019-10-07 12:28:54 -0700

    test gql lambda

---
+++ functions/graphql/test.js
@@ -0,0 +1,25 @@
+import lambda from 'apollo-server-lambda';
+const { ApolloServer, gql } = lambda;
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+export default server.createHandler({
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
