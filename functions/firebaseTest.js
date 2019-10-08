7f4c8a31d262a170353ad5df19ba6a6740e9b8bb
JeffML <jlowery2663@gmail.com>
2019-10-07 20:14:42 -0700

    WIP: strip and paste

--- functions/firebaseTest.js
+++ functions/firebaseTest.js
@@ -1,7 +1,7 @@
 const Firestore = require('@google-cloud/firestore');
 
 // // Create a new client
-// const firestore = new Firestore();
+const firestore = new Firestore();
 
 // async function quickstart() {
 //   // Obtain a document reference.
