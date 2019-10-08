5c9c9b54a265bb40cf12cc7040e35efa0d78d58e
JeffML <jlowery2663@gmail.com>
2019-10-07 20:04:59 -0700

    WIP: strip and paste

--- functions/firebaseTest.js
+++ functions/firebaseTest.js
@@ -1,25 +1,25 @@
-const Firestore = require('@google-cloud/firestore');
+// const Firestore = require('@google-cloud/firestore');
 
-// Create a new client
-const firestore = new Firestore();
+// // Create a new client
+// const firestore = new Firestore();
 
-async function quickstart() {
-  // Obtain a document reference.
-  const document = firestore.doc('posts/intro-to-firestore2');
+// async function quickstart() {
+//   // Obtain a document reference.
+//   const document = firestore.doc('posts/intro-to-firestore2');
 
-  // Enter new data into the document.
-  await document.set({
-    title: 'Welcome to Firestore2',
-    body: 'Hello World2',
-  });
-  console.log('Entered new data into the document');
-}
+//   // Enter new data into the document.
+//   await document.set({
+//     title: 'Welcome to Firestore2',
+//     body: 'Hello World2',
+//   });
+//   console.log('Entered new data into the document');
+// }
 
 exports.handler = (event, context, callback) => {
-  quickstart().then(() => callback(null, {
-    statusCode: 200,
-    body: "foo",
-  }))
-    .catch(e => callback(e));
-
+  // quickstart().then(() => callback(null, {
+  //   statusCode: 200,
+  //   body: "foo",
+  // }))
+  //   .catch(e => callback(e));
+  callback(null, { statusCode: 200, body: "goob" })
 }
