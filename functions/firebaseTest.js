770bf99a6e66e1740d25a3681df6ced6d6cb40b7
JeffML <jlowery2663@gmail.com>
2019-10-07 20:17:24 -0700

    WIP: strip and paste

--- functions/firebaseTest.js
+++ functions/firebaseTest.js
@@ -7,12 +7,12 @@ async function quickstart() {
   // Obtain a document reference.
   const document = firestore.doc('posts/intro-to-firestore2');
 
-  // Enter new data into the document.
-  await document.set({
-    title: 'Welcome to Firestore2',
-    body: 'Hello World2',
-  });
-  console.log('Entered new data into the document');
+  // // Enter new data into the document.
+  // await document.set({
+  //   title: 'Welcome to Firestore2',
+  //   body: 'Hello World2',
+  // });
+  // console.log('Entered new data into the document');
 }
 
 exports.handler = (event, context, callback) => {
