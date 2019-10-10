a47a0606d2865cb5e7a9f412de8e532af2b42799
JeffML <jlowery2663@gmail.com>
2019-10-09 20:34:20 -0700

    copy auth file to output

--- functions/firebaseTest.js
+++ functions/firebaseTest.js
@@ -1,4 +1,5 @@
 const Firestore = require('@google-cloud/firestore');
+require('./pgnfen2-0dcc7c57af09.json')
 
 // // Create a new client
 const firestore = new Firestore();
--- webpack.functions.js
+++ webpack.functions.js
@@ -1,5 +1,11 @@
 const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   externals: [nodeExternals()],
+  plugins: [
+    new CopyPlugin([
+      { from: 'pgnfen2-0dcc7c57af09.json', to: '../functions-build' },
+    ]),
+  ],
 };
