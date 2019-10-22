7231af6d98139ad109fdf40879a8a0c54cd06efd
JeffML <jlowery2663@gmail.com>
2019-10-22 12:28:22 -0700

    fbadmin

--- functions/firebaseTest.js
+++ functions/firebaseTest.js
@@ -1,23 +1,30 @@
-const Firestore = require('@google-cloud/firestore');
-require('creds')
+const admin = require('firebase-admin')
+// const Firestore = require('@google-cloud/firestore');
+const credential = require('./pgnfen2-0dcc7c57af09.json')
 
-// // Create a new client
-const firestore = new Firestore();
 
-async function quickstart() {
-  // Obtain a document reference.
-  const document = firestore.doc('posts/intro-to-firestoreeee');
+admin.initializeApp({
+  credential: admin.credential.cert(credential)
+});
 
-  // // Enter new data into the document.
-  await document.set({
-    title: 'Welcome to Firestore2',
-    body: 'Hello World2',
-  });
-  // console.log('Entered new data into the document');
-}
+// // // Create a new client
+// const firestore = new Firestore();
 
-exports.handler = async (event, context, callback) => {
-  await quickstart().catch(e => { throw e })
-  return { statusCode: 200, body: "okay" }
-}
-exports.quickstart = quickstart;
+// async function quickstart() {
+//   // Obtain a document reference.
+//   const document = firestore.doc('posts/intro-to-firestoreeee');
+
+//   // // Enter new data into the document.
+//   await document.set({
+//     title: 'Welcome to Firestore2',
+//     body: 'Hello World2',
+//   });
+//   // console.log('Entered new data into the document');
+// }
+
+// exports.handler = async (event, context, callback) => {
+//   await quickstart().catch(e => { throw e })
+//   return { statusCode: 200, body: "okay" }
+// }
+// exports.quickstart = quickstart;
+exports.admin = admin;
