af5c3259088a51d4ca07dd43459569e5385f5587
JeffML <jlowery2663@gmail.com>
2019-11-01 17:50:38 -0700

    baby, it's working!

--- functions/pgnfen/resolvers.js
+++ functions/pgnfen/resolvers.js
@@ -1,13 +1,28 @@
 const openings = require('./scid.js');
 
+
 const fetchGames = () => null;
 const addOpenings = async (_, { start, end }, { admin }) => {
-  const times = await admin.firestore()
-    .doc('chess/openings/fen/f1')
-    .set({ fen: 'f1', SCID: 'scid1', desc: 'howdy' });
-    // .set(openings.slice(start, end));
-  console.dir(times);
-  return times._writeTime._seconds;
+  const db = admin.firestore();
+  const batch = db.batch();
+  const fens = db.collection('chess/openings/fen');
+  const data = openings.slice(start, end);
+
+  data.forEach((opening) => {
+    const id = opening.fen.replace(/\//g, '$');
+    const doc = fens.doc(id);
+    batch.set(doc, opening);
+  });
+
+  const times = await batch.commit();
+
+  //   const times = await admin.firestore()
+  //     .doc('chess/openings/fen/f1')
+  //     .set({ fen: 'f1', SCID: 'scid1', desc: 'howdy' });
+  //     // .set(openings.slice(start, end));
+  console.dir(times[0]);
+  return times[0]._writeTime._seconds;
 };
 
 module.exports = { fetchGames, addOpenings };
+git;
